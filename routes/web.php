e25c0baeccdce7da439603a5d62de75bdf21ca71
ajonmarc <cortelaljonmarc@gmail.com>
2026-02-02 15:38:27 +0800

    added api

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,10 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('/test', function () {
+    return 'Laravel is alive';
+});
+
+
 require __DIR__.'/settings.php';
